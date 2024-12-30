25ff3a52ee8ba8fc233882ab2adc0c78a9a21042
Carlos Cepero <ceperocarlosraul@gmail.com>
2024-12-30 04:02:54 -0500

    backend-terminado

--- config/db.js
+++ config/db.js
@@ -3,11 +3,10 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true, // Configuración para la URI
-      useUnifiedTopology: true, 
-      serverSelectionTimeoutMS: 5000, 
-      socketTimeoutMS: 45000, 
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
+
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
